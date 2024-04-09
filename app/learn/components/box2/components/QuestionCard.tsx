ae699bf830282c751678c43b3317dfce42761cd0
sandervaneck <69575096+sandervaneck@users.noreply.github.com>
2024-04-09 13:25:58 +0200

    setQuestions

--- app/learn/components/box2/components/QuestionCard.tsx
+++ app/learn/components/box2/components/QuestionCard.tsx
@@ -2,21 +2,31 @@ import { Grid, Card, Typography, Checkbox, Button} from '@mui/material';
 import { Question, Questions } from './Questions';
 import React, { useState } from 'react';
 
-export const QuestionCard = ({questions}: {questions: Questions}) => {
+interface QuestionsCardProps {
+    questions: Questions;
+    setQuestions: (q: Questions) => void;
+}
+interface QuestionProps {
+    question: Question;
+    questions: Questions;
+    setQuestions: (q: Questions) => void;
+    index: number;
+}
+export const QuestionCard: React.FC<QuestionsCardProps> = ({questions, setQuestions}) => {
 
     return (
         <Card>
                     <Typography variant="h6" align="center">{questions.title}</Typography>
-                    <QuestionsStepper questions={questions.questions}/>
+                    <QuestionsStepper questions={questions.questions} setQuestions={setQuestions}/>
         </Card>
     )
 }
 
-const QuestionsStepper = ({questions}: {questions: Question[]}) => {
+const QuestionsStepper: React.FC<QuestionsCardProps> = ({questions, setQuestions}) => {
 const [index, setindex] = useState(0)
     return (
         <>
-        <QuestionAsked question={questions[index]} index={index}/>
+        <QuestionAsked question={questions[index]} questions={questions} setQuestions={setQuestions} index={index}/>
         <Button variant="outlined" onClick={() => {
             if (index - 1 >= 0) setindex(index + 1)}
         }>Back</Button>
@@ -26,13 +36,21 @@ const [index, setindex] = useState(0)
         </>
     )
 }
-const QuestionAsked = ({question, index}: {question: Question, index: number}) => {
+
+const QuestionAsked: React.FC<QuestionProps> = ({question, questions, index, setQuestions}) => {
     return (    
     <Grid container xs={12} key={index}>
-                            <Grid item xs={12}>{question.question}</Grid>
+                            <Grid item xs={12}><Typography variant="subtitle" align="left">{`(${index + 1}) ${question.question}`}</Typography></Grid>
                             {question.options.map((option, i) => (
                                 <Grid container xs={12} key={i}>
-                                <Grid item xs={1}><Checkbox checked={false} onChange={() => {}}/></Grid>
+                                <Grid item xs={1}><Checkbox checked={false} onChange={() => {
+                                    const old = [...questions]
+                                    old[index] = {
+                                        ...old[index],
+                                        answer: option
+                                    }
+                                    setQuestions(old)
+                                }}/></Grid>
                                 <Grid item xs={11}>{option}</Grid>
                                     </Grid>
                             ))}
--- app/learn/components/box2/index.tsx
+++ app/learn/components/box2/index.tsx
@@ -1,14 +1,14 @@
 import { Grid, Card} from '@mui/material';
 import { QuestionCard } from './components/QuestionCard';
 import { questions1 } from './components/Questions';
-
-
+import React, { useState } from 'react';
 
 export const Box2 = () => {
+    const [questions, setQuestions] = useState(questions1)
     return (<Card>
         <Grid container>
             <Grid item xs={12}>
-                <QuestionCard questions={questions1} />
+                <QuestionCard questions={questions} setQuestions={setQuestions}/>
             </Grid>
             </Grid>
             </Card>)
